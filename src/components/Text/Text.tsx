8a8ddaf9df4a6c517649cb2d177e8de9650f1c82
cannonfodderalt <cannonfodderalt@gmail.com>
2025-09-19 15:57:52 -0700

    add text type component

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,17 @@
+type TextVariant = "title" | "subtitle" | "body";
+
+interface TextProps {
+  variant?: TextVariant;
+  children: React.ReactNode;
+}
+
+export function Text({variant = "body", children}: TextProps) {
+  switch (variant) {
+    case "title":
+      return <h1 className="text-4xl font-bold">{children}</h1>;
+    case "subtitle":
+      return <h2 className="text-2xl font-medium">{children}</h2>;
+    case "body":
+      return <p>{children}</p>;
+  }
+}
