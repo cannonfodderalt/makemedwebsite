3217b075d5349f560cde11cc4bfce2afd79fa32f
cannonfodderalt <cannonfodderalt@gmail.com>
2025-09-28 13:49:13 -0700

    remove about

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import {Text} from "@/components/Text/Text";
-
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-primary">
-        <Text variant="title">about page</Text>
-    </div>
-  );
-}
